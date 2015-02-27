c466908de3d5cf4ad6d8a5af7f99680be360df7d
Nyhilo <silentclowd@gmail.coom>
2015-02-26 17:06:47 -0700

    New Assignment 2_2
    Created the framework for Assignment 2_2, a text-based calculator
    program

---
+++ Assignment 2_2/Assignment 2_2/Calculator.cpp
@@ -0,0 +1,10 @@
+/*
+File containing function definitions fo the main program.
+
+~Jacob Cloward
+2/26/15
+*/
+
+// Includes
+
+// Function Definitions
---
+++ Assignment 2_2/Assignment 2_2/Calculator.h
@@ -0,0 +1,8 @@
+/*
+Header file containing function prototypes, among other things.
+
+~Jacob Cloward
+2/26/15
+*/
+
+// Function Prototypes
